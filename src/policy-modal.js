76e8c3084fe9cd66e8365929ec00886136869d2b
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-09-02 11:55:49 +0300

    added scroll handling

--- src/policy-modal.js
+++ src/policy-modal.js
@@ -9,18 +9,25 @@
   refs.closeModalButtons.forEach((element) => element.addEventListener('click', toggleModal));
   refs.modal.addEventListener('click', closeModal);
 
+  function toggleBodyScroll() {
+    document.documentElement.classList.toggle('no-scroll');
+  }
+
   function closeModal(e) {
     if (e.code === 'Escape') {
       refs.modal.classList.add('is-hidden');
       document.removeEventListener('keydown', closeModal);
+      toggleBodyScroll();
     }
     if (e.target === refs.modal) {
       refs.modal.classList.toggle('is-hidden');
+      toggleBodyScroll();
     }
   }
 
   function toggleModal() {
     window.addEventListener('keydown', closeModal);
     refs.modal.classList.toggle('is-hidden');
+    toggleBodyScroll();
   }
 })();
